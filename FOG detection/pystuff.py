dbf52d54d55a60bbb450f28873c7dc542c3db5b0
Grossadmiral <DrWhilsonMFI@gmail.com>
2024-03-09 21:14:43 +0700

    LSTM with Dropout

--- FOG detection/pystuff.py
+++ FOG detection/pystuff.py
@@ -13,13 +13,16 @@ X_train = keras.preprocessing.sequence.pad_sequences(X_train, maxlen=max_review_
 X_test = keras.preprocessing.sequence.pad_sequences(X_test, maxlen=max_review_length)
 
 embedding_vector_length: int = 32
+
 model = keras.Sequential()
 model.add(layers.Embedding(top_words, embedding_vector_length))
+model.add(layers.Dropout(0.2))
 model.add(layers.LSTM(100))
+model.add(layers.Dropout(0.2))
 model.add(layers.Dense(1, activation='sigmoid'))
 model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
 print(model.summary())
-model.fit(X_train, y_train, epochs=3, batch_size=64)
+model.fit(X_train, y_train, epochs=7, batch_size=64)
 
 scores = model.evaluate(X_test, y_test, verbose=0)
 print("Accuracy: %.2f%%" % (scores[1] * 100))
