58ce344ca2e6cd19a57f7ab10724d3624b68ec62
Grossadmiral <DrWhilsonMFI@gmail.com>
2024-03-09 20:47:36 +0700

    example now work

--- FOG detection/pystuff.py
+++ FOG detection/pystuff.py
@@ -12,14 +12,14 @@ max_review_length = 500
 X_train = keras.preprocessing.sequence.pad_sequences(X_train, maxlen=max_review_length)
 X_test = keras.preprocessing.sequence.pad_sequences(X_test, maxlen=max_review_length)
 
-embedding_vecor_length = 32
+embedding_vector_length: int = 32
 model = keras.Sequential()
-model.add(keras.layers.Embedding(top_words, embedding_vecor_length, input_length=max_review_length))
-model.add(keras.layers.LSTM(100))
-model.add(keras.layers.Dense(1, activation='sigmoid'))
+model.add(layers.Embedding(top_words, embedding_vector_length))
+model.add(layers.LSTM(100))
+model.add(layers.Dense(1, activation='sigmoid'))
 model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
 print(model.summary())
 model.fit(X_train, y_train, epochs=3, batch_size=64)
 
 scores = model.evaluate(X_test, y_test, verbose=0)
-print("Accuracy: %.2f%%" % (scores[1]*100))
+print("Accuracy: %.2f%%" % (scores[1] * 100))
