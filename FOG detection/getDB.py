852f818c3f838ec6cfbd50af5d2c454d09b63664
Grossadmiral <DrWhilsonMFI@gmail.com>
2024-04-27 00:51:39 +0700

    Add WindowGenerator class

--- FOG detection/getDB.py
+++ FOG detection/getDB.py
@@ -8,6 +8,46 @@ from sklearn.model_selection import train_test_split
 import os
 
 
+class WindowGenerator:
+  def __init__(self, input_width, label_width, shift,
+               train_df, val_df, test_df,
+               label_columns=None):
+    # Store the raw data
+    self.train_df = train_df
+    self.val_df = val_df
+    self.test_df = test_df
+
+    # Work out the label column indices
+    self.label_columns = label_columns
+    if label_columns is not None:
+      self.label_columns_indices = {name: i for i, name in
+                                    enumerate(label_columns)}
+    self.column_indices = {name: i for i, name in
+                           enumerate(train_df.columns)}
+
+    # Work out the window parameters
+    self.input_width = input_width
+    self.label_width = label_width
+    self.shift = shift
+
+    self.total_window_size = input_width + shift
+
+    self.input_slice = slice(0, input_width)
+    self.input_indices = np.arange(self.total_window_size)[self.input_slice]
+
+    self.label_start = self.total_window_size - self.label_width
+    self.labels_slice = slice(self.label_start, None)
+    self.label_indices = np.arange(self.total_window_size)[self.labels_slice]
+
+
+  def __repr__(self):
+    return '\n'.join([
+        f'Total window size: {self.total_window_size}',
+        f'Input indices: {self.input_indices}',
+        f'Label indices: {self.label_indices}',
+        f'Label column name(s): {self.label_columns}'])
+
+
 def get_tdcsfog_full(super_folder_path, folder_path, tdcsfog_path):
     sample_cs_path = os.path.join(super_folder_path, folder_path, tdcsfog_path)
     tdcsfog_df = []
@@ -156,12 +196,10 @@ def get_tr_val_tst_data(all_train_data, all_features, lookback, targets):
         if Id in test_id:
             test = pd.concat([test, group])
 
-    # Vive result info
+    # Vive data describe
     print(train.info)
     print(val.info)
     print(test.info)
-
-    print(len(train_id), ' - ', len(train['Id'].unique()))
 
     return train, val, test
 
