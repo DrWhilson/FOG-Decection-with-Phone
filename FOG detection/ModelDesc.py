20ef343d782fac38157128f3385ff36dc91f698f
Grossadmiral <DrWhilsonMFI@gmail.com>
2024-05-12 20:10:23 +0700

    Inherit the model

--- FOG detection/ModelDesc.py
+++ FOG detection/ModelDesc.py
@@ -3,10 +3,9 @@ from tensorflow.keras import Sequential
 from tensorflow.keras.layers import Dense, Dropout, Conv1D, Flatten, LSTM
 
 
-class LSTMModel:
-    model = Sequential()
-
+class LSTMModel(tf.keras.Model):
     def __init__(self, wide_window, features, batch_size=32):
+        super(LSTMModel, self).init()
         tf.config.run_functions_eagerly(True)
 
         input_shape = None
@@ -14,19 +13,20 @@ class LSTMModel:
         for inputs, targets in wide_window.train.take(1):
             input_shape = inputs.shape[1:]
 
-        model = Sequential(name='Prediction_Gait')
-        model.add(LSTM(80, input_shape=input_shape, return_sequences=True))
+        self.model = Sequential(name='Prediction_Gait')
+        self.model.add(LSTM(80, input_shape=input_shape, return_sequences=True))
 
-        model.add(LSTM(128, activation='relu'))
-        model.add(Dropout(0.2))
+        self.model.add(LSTM(128, activation='relu'))
+        self.model.add(Dropout(0.2))
 
-        model.add(Dense(80, activation='relu'))
-        model.add(Dense(64, activation='relu'))
-        model.add(Dense(32, activation='relu'))
+        self.model.add(Dense(80, activation='relu'))
+        self.model.add(Dense(64, activation='relu'))
+        self.model.add(Dense(32, activation='relu'))
 
-        model.add(Dense(10, activation='sigmoid'))
-        model.add(Dropout(0.2))
+        self.model.add(Dense(10, activation='sigmoid'))
+        self.model.add(Dropout(0.2))
 
-        model.add(Dense(4, activation='sigmoid'))
+        self.model.add(Dense(4, activation='sigmoid'))
 
-        self.model = model
+    def call(self, inputs):
+        return self.model(inputs)
